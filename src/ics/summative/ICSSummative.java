2e5e4c04d8f18d0c7b9feb6ca16d4c8bf18cf524
felix <felix@DESKTOP-D0PQSH8>
2025-06-15 22:15:57 -0400

    Final push before submit

--- src/ics/summative/ICSSummative.java
+++ src/ics/summative/ICSSummative.java
@@ -4,6 +4,9 @@
  */
 package ics.summative;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author felix
@@ -13,8 +16,201 @@ public class ICSSummative {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static Scanner input = new Scanner(System.in);
     
+    public static void main(String[] args) {
+        while (true) {
+            Game game = new Game();
+            game.startRace();
+
+            System.out.print(" Do you want to race again? (y/n): ");
+            String again = input.next().toLowerCase();
+
+            if (!again.equals("y")) {
+                System.out.println(" Thanks for playing!");
+                break;
+            }
+        }
+    }
+}
+
+// Base class (inheritance)
+class Animal {
+    protected String name;
+    protected int position = 0;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    // Overloaded constructor
+    public Animal(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public void move() {
+        position += (int) (Math.random() * 3) + 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String toString() {
+        return name + " is at position " + position;
+    }
+}
+
+class FastAnimal extends Animal {
+    public FastAnimal(String name) {
+        super(name);
+    }
+
+    @Override
+    public void move() {
+        position += (int) (Math.random() * 4) + 3;
+    }
+}
+
+class Game {
+    private Animal[] racers = new Animal[12];
+    private static int raceCount = 0;
+    private String[][] track = new String[12][21];
+
+    public Game() {
+        racers[0] = new FastAnimal("Rat");
+        racers[1] = new Animal("Ox");
+        racers[2] = new FastAnimal("Tiger");
+        racers[3] = new Animal("Rabbit");
+        racers[4] = new Animal("Dragon");
+        racers[5] = new FastAnimal("Snake");
+        racers[6] = new Animal("Horse");
+        racers[7] = new Animal("Goat");
+        racers[8] = new Animal("Monkey");
+        racers[9] = new Animal("Rooster");
+        racers[10] = new Animal("Dog");
+        racers[11] = new Animal("Pig");
+    }
+
+    public void startRace() {
+        raceCount++;
+        System.out.println("Welcome to the Zodiac Race!");
+
+        System.out.println("Choose your animal:");
+        for (int i = 0; i < racers.length; i++) {
+            System.out.println((i + 1) + ". " + racers[i].getName());
+        }
+
+        int choice = ICSSummative.input.nextInt() - 1;
+        Animal playerAnimal = racers[choice];
+
+        Animal betAnimal = placeBet();
+
+        boolean raceOver = false;
+        long startTime = System.currentTimeMillis();
+
+        while (!raceOver) {
+            for (int i = 0; i < racers.length; i++) {
+                racers[i].move();
+                if (racers[i].getPosition() >= 20) {
+                    raceOver = true;
+                }
+            }
+
+            displayTrack();
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {}
+        }
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println(" Race time: " + duration / 1000.0 + " seconds");
+
+        Animal winner = getWinner();
+        System.out.println(" The winner is: " + winner.getName());
+
+        if (winner.getName().equals(playerAnimal.getName())) {
+            System.out.println(" You won the race!");
+        } else {
+            System.out.println(" Better luck next time!");
+        }
+
+        if (winner.getName().equals(betAnimal.getName())) {
+            System.out.println(" You WON your bet on " + betAnimal.getName() + "!");
+        } else {
+            System.out.println(" You lost your bet on " + betAnimal.getName() + ".");
+        }
+
+        saveToFile(winner.getName());
+        readFromFile();
+    }
+
+    public Animal placeBet() {
+        System.out.println("Place your bet! Which animal do you think will win?");
+        for (int i = 0; i < racers.length; i++) {
+            System.out.println((i + 1) + ". " + racers[i].getName());
+        }
+        int betChoice = ICSSummative.input.nextInt() - 1;
+        return racers[betChoice];
+    }
+
+    public void displayTrack() {
+        for (int i = 0; i < racers.length; i++) {
+            for (int j = 0; j < 21; j++) {
+                track[i][j] = " ";
+            }
+            int pos = Math.min(racers[i].getPosition(), 20);
+            track[i][pos] = racers[i].getName().substring(0, 1);
+        }
+
+        for (int i = 0; i < track.length; i++) {
+            System.out.print(racers[i].getName() + ": ");
+            for (int j = 0; j < track[i].length; j++) {
+                System.out.print(track[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------");
+    }
+
+    public Animal getWinner() {
+        Animal winner = racers[0];
+        for (Animal a : racers) {
+            if (a.getPosition() > winner.getPosition()) {
+                winner = a;
+            }
+        }
+        return winner;
+    }
+
+    public void saveToFile(String winnerName) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("race_results.txt", true));
+            writer.println("Race #" + raceCount + ": " + winnerName + " won!");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("File write error.");
+        }
+    }
+
+    public void readFromFile() {
+        System.out.println("Previous Race Results:");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("race_results.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(" - " + line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("No past results found.");
+        }
+    }
 }
--- src/ics/summative/NewJFrame.java
+++ src/ics/summative/NewJFrame.java
@@ -4,6 +4,11 @@
  */
 package ics.summative;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 /**
  *
  * @author felix
@@ -26,21 +31,137 @@ public class NewJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jLabel1 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Choose your Animal");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Place your bet on:");
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" }));
+        jComboBox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox2ActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Start Race");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
+        jTextArea1.setRows(5);
+        jScrollPane2.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(151, 151, 151)
+                        .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(67, 67, 67)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(35, 35, 35)
+                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(5, 5, 5)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(5, 5, 5)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(28, 28, 28)
+                .addComponent(jButton1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(47, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:       
+// Get the selected animal (the one that "runs the race")
+String selectedAnimal = (String) jComboBox1.getSelectedItem();
+// Get the user's bet
+String betAnimal = (String) jComboBox2.getSelectedItem();
+
+// Create a list of all animals for the race
+String[] animals = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+
+// Use Random to pick a winning animal
+Random rand = new Random();
+String winningAnimal = animals[rand.nextInt(animals.length)];
+
+// Build output message
+StringBuilder output = new StringBuilder();
+output.append("Race starting!\n");
+output.append("Your animal: ").append(selectedAnimal).append("\n");
+output.append("Your bet: ").append(betAnimal).append("\n");
+output.append("Winning animal: ").append(winningAnimal).append("\n");
+
+// Check if bet matches the winner
+if (betAnimal.equals(winningAnimal)) {
+    output.append("Congratulations! You won the bet!");
+} else {
+    output.append("Sorry, you lost the bet.");
+}
+
+// Display result in the text area
+jTextArea1.setText(output.toString());
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jComboBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +199,14 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
